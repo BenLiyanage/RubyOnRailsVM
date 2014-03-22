60020e9b86686d38da0e76ee5b5c3cd92f18f878
Ben Liyanage <git@perfectresolution.com>
2014-03-22 14:50:25 -0400

    initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OpenCamp::Application.config.secret_key_base = '868c6f93b67a98b39e5662bd159fcd9e04e404adf383f4bed514abb4576fcf8ff56d8593408b0975376a68e21032cc5a22448c70192f1851e9576e698d114566'
